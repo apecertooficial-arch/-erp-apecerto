f712686242a72a29295dbd63c92293b8624236bd
apecertooficial-arch <apecerto.oficial@gmail.com>
2026-08-18 17:24:33 -0300

    Privacidade 9a: remove import nao usado (Valor) para nao quebrar o typecheck

--- app/features/inteligencia/telas/PrivacidadeTracking.tsx
+++ app/features/inteligencia/telas/PrivacidadeTracking.tsx
@@ -20,7 +20,7 @@
  */
 
 import type { PropsTela } from "../CascaInteligencia";
-import { fmt, RodapeFontes, Valor } from "../dado";
+import { fmt, RodapeFontes } from "../dado";
 import { Cabecalho, ChipsEventos } from "../pecas";
 
 type Estado = "bom" | "aviso";
